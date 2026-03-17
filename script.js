5943feede7be56bafbb9b4c11e2a50827aa43603
Elisheva <ekinzer@wits.edu>
2026-03-17 13:26:49 -0400

    The program starts off with a question

--- script.js
+++ script.js
@@ -15,9 +15,12 @@ fetch("flashcardSet.json")
     .then(data => {
         for(let card of data.flashcardSet.cards){
             cardArray.push(card);
-        }     
+        } 
+    let x = Math.floor((Math.random() * 10));
+    let cardNow = cardArray[x];
+    questionText.innerHTML = cardNow.question;
+    answerText.innerHTML = cardNow.answer;    
 })
-
 
 
 function flipcard(){
